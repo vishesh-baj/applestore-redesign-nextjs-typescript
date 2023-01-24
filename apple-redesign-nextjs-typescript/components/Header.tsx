949b9052b9a0224a8c59e3ce9de2040dc49c7308
vishesh-baj <vishesh.bajpayee@icloud.com>
2023-01-24 15:38:43 +0530

    added changes to header component and created a new landing page

--- apple-redesign-nextjs-typescript/components/Header.tsx
+++ apple-redesign-nextjs-typescript/components/Header.tsx
@@ -34,7 +34,7 @@ const Header = () => {
         <SearchIcon className="headerIcon" />
         <Link href="/checkout">
           <div className="relative cursor-pointer">
-            <span className="absolute -right-1 -top-1 z-40 flex h-4 w-4 items-center justify-center rounded-full bg-gradient-to-r from-pink-500 to-violet-500 text-white">
+            <span className="absolute -right-1 -top-1 z-40 flex h-4 w-4 items-center justify-center rounded-full bg-gradient-to-r from-pink-500 to-violet-500 text-[10px] text-white">
               5
             </span>
             <ShoppingBagIcon className="headerIcon" />
--- apple-redesign-nextjs-typescript/pages/index.tsx
+++ apple-redesign-nextjs-typescript/pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Header from "../components/Header";
+import Landing from "../components/Landing";
 
 const Home: NextPage = () => {
   return (
@@ -10,6 +11,10 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
+
+      <main>
+        <Landing />
+      </main>
     </div>
   );
 };
